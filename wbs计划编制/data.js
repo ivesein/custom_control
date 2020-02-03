5c6a343f1efdbf88cf45360181d088f6c49d3eed
ivesein <ivesein@outlook.com>
2020-02-03 22:26:33 +0800

    fix

---
+++ wbs计划编制/data.js
@@ -0,0 +1,43 @@
+var data={
+    "planned_end": "2019-10-13 00:00:00",//计划结束时间
+    "latest_start_time": "2019-10-03 00:00:00",//最迟开始时间
+    "task_after": "",//紧后任务
+    "check_id":"",//被审核任务的id
+    "owner_id": ["23738b34-7b9c-4fa4-a59c-3369de6700210", "23738b34-7b9c-4fa4-a59c-3369de6700211", "23738b34-7b9c-4fa4-a59c-3369de6700212"],//资源id
+    "planned_start": "2019-10-06 00:00:00",
+    "wbs": "1.1.1",
+    "position": "3",
+    "latest_end_time": "2019-10-17 18:30:02",
+    "parallel_work": "2,4",//平行工作
+    "free_float": 0,//自由时差
+    "parent": "",
+    "open": true,
+    "lap_joint_work": "",//搭接工作
+    "start_date": "2019-10-03 00:00:00",
+    "describe": [],//描述
+    "review_task": [],//审核任务的id
+    "text": "demo",
+    "progress": 0.15833333,
+    "isCriticalTask": true,
+    "effect": "0.73",//工效
+    "end_date": "2019-10-17 18:30:02",
+    "duration": 11,
+    "task_type": "",//任务类型
+    "duration_plan": "8",
+    "total_slack": 0,
+    "my_unit": {
+            "text": "",  //单位名称
+            "id":""    //单位id
+    },
+    "major": {
+        "text": "",  //专业名称
+        "id":""    //专业id
+    },
+    "earliest_time": "2019-10-03 00:00:00",
+    "task_before": "",//紧前任务
+    "type": "project",
+    "id": "23738b34-7b9c-4fa4-a59c-3369de670022",
+    "constraint_type": "asap",
+    "calendar_id": "23738b34-7b9c-4fa4-a59c-3369de670021",//设置任务例外需要用到
+    "earliest_end_time": "2019-10-17 18:30:02"
+}
