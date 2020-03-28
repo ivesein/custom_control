313bb7a08c97f317446bc95ff44a6fbf0b5d7d5d
ivesein <ivesein@outlook.com>
2020-03-28 18:59:20 +0800

    mfix quality_plan resource_allowcation and add progress_ac

---
+++ 执行管理-进度管理-进度分析及控制/进度分析控制表/progress_contrast_analysis_1.0/js/test.js
@@ -0,0 +1,129 @@
+new Vue({
+  delimiters: ["${", "}"],
+  data: {
+    ifSelectBoxShow:false,
+    proTableData:[
+      {
+        wbs:"G",
+        task_name:"工作5",
+        owner:"张三",
+        is_certical_task:true,
+        task_status:"1",  //1 未开始  2 进行中  3 已完成
+        handle_status:"0"  // 0 待处理  1 已处理
+      },
+      {
+        wbs:"H",
+        task_name:"工作6",
+        owner:"李四",
+        is_certical_task:true,
+        task_status:"2",
+        handle_status:"0"
+      },
+      {
+        wbs:"L",
+        task_name:"工作7",
+        owner:"王五",
+        is_certical_task:false,
+        task_status:"3",
+        handle_status:"1"   
+      },
+      {
+        wbs:"I",
+        task_name:"工作8",
+        owner:"马六",
+        is_certical_task:true,
+        task_status:"3",
+        handle_status:"1"   
+      }
+    ],
+    beforeTaskInfo:{
+      open:true,
+      title:"紧前任务对当前任务影响情况",
+      data:[
+        {before_task_name:"工作4",handling_measures:"成本压缩100元"}
+      ]
+    },
+    currentTaskInfo:{
+      title:"当前任务情况",
+      task_status:"1",
+      open:true,
+      data:[
+        {
+          plan_duration:"10", //计划工期
+          plan_quantities:"150", //计划工程量
+          duration_after_adjustment:"12", //调整后工期
+          actual_duration:"15",  //实际工期
+          actual_quantities:"170",  //实际工程量
+          free_time:0,  //自由时差
+          free_time_exceed:0, //超出自由时差
+          free_time_analysis_conclusion:"自由时差分析结论", //自由时差分析结论 
+          total_time:0, //总时差
+          // plan_cost:250,
+          // cost_after_adjustment:150,
+          // actual_cost:100
+          total_time_exceed:0, //超出总时差
+          total_time_analysis_conclusion:"总时差分析结论", //总时差分析结论 
+        }
+      ]
+    },
+    earnedValueAnalysisSuggestions:{
+      title:"挣值分析及建议",
+      open:true,
+      data:[],
+    },
+    followTaskProcessing:{
+      title:"后续任务处理",
+      open:true,
+      data:[
+        {
+          follow_task_name:"",
+          handling_measures:"请输入处理措施"
+        }
+      ]
+    }
+  },
+  created() {
+   
+  },
+  mounted() {
+    // this.$nextTick(function() {
+    //   this.tableHeight = window.innerHeight - this.$refs.qualityPlanTable.$el.offsetTop - 10
+
+    //   // 监听窗口大小变化
+    //   let self = this;
+    //   window.onresize = function() {
+    //     self.tableHeight = window.innerHeight - self.$refs.qualityPlanTable.$el.offsetTop - 10
+    //   }
+    // })
+  },
+  methods: {
+    refreshData(){},
+    goExit(){},
+    goBeforeTaskDetail(){},
+    goCurrentTaskControl(){},
+    goTaskRTeportDetail(){},
+    goProgress(){},
+    showSelectBox(){
+      this.ifSelectBoxShow=!this.ifSelectBoxShow
+    },
+    cellStyle({ row, column, rowIndex, columnIndex }) {
+      if (columnIndex === 0) {
+        // console.log(column)
+        return "padding: 0px!important;"
+      }
+
+    },
+    iconArrowClick1(data){
+      data.open=!data.open
+    },
+    iconArrowClick2(data){
+      data.open=!data.open
+    },
+    iconArrowClick3(data){
+      data.open=!data.open
+    },
+    iconArrowClick4(data){
+      data.open=!data.open
+    }
+  }
+}).$mount("#progressCAApp")
