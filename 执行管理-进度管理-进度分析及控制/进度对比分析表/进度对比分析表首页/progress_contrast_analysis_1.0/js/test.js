6dc456043e4b7a423f98c24cf388003397a892d1
ivesein <ivesein@outlook.com>
2020-04-08 19:00:46 +0800

    edit progressss_contrast_analysis

--- 执行管理-进度管理-进度分析及控制/进度对比分析表/进度对比分析表首页/progress_contrast_analysis_1.0/js/test.js
+++ 执行管理-进度管理-进度分析及控制/进度对比分析表/进度对比分析表首页/progress_contrast_analysis_1.0/js/test.js
@@ -2,14 +2,15 @@ new Vue({
   delimiters: ["${", "}"],
   data: {
     ifSelectBoxShow:false,
+    // 任务表数据结构
     proTableData:[
       {
-        wbs:"G",
-        task_name:"工作5",
-        owner:"张三",
-        is_certical_task:true,
-        task_status:"1",  //1 未开始  2 进行中  3 已完成
-        handle_status:"0"  // 0 待处理  1 已处理
+        wbs:"G",  //wbs任务代码
+        task_name:"工作5",  //wbs任务名称
+        owner:"张三",   //承担人
+        is_certical_task:true,  //是否关键任务
+        task_status:"1",  //任务状态  1 未开始  2 进行中  3 已完成
+        handle_status:"0"  //处理状态  0 待处理  1 已处理
       },
       {
         wbs:"H",
@@ -36,19 +37,27 @@ new Vue({
         handle_status:"1"   
       }
     ],
+    //紧前任务对当前任务影响情况表数据结构
     beforeTaskInfo:{
       open:true,
       title:"紧前任务对当前任务影响情况",
       data:[
-        {before_task_name:"工作4",handling_measures:"成本压缩100元"}
+        {
+          before_task_name:"工作4",  //前置任务名称
+          handling_measures:"成本压缩100元" //当前任务处理措施
+        }
       ]
     },
+    //当前任务情况表数据结构
     currentTaskInfo:{
       title:"当前任务情况",
       task_status:"1",
       open:true,
       data:[
         {
+          task_id:"1",  //任务id
+          task_name:"p4挖土", //任务名称
+          task_status:"1",  //任务状态
           plan_duration:"10", //计划工期
           plan_quantities:"150", //计划工程量
           duration_after_adjustment:"12", //调整后工期
@@ -58,9 +67,6 @@ new Vue({
           free_time_exceed:0, //超出自由时差
           free_time_analysis_conclusion:"自由时差分析结论", //自由时差分析结论 
           total_time:0, //总时差
-          // plan_cost:250,
-          // cost_after_adjustment:150,
-          // actual_cost:100
           total_time_exceed:0, //超出总时差
           total_time_analysis_conclusion:"总时差分析结论", //总时差分析结论 
         }
@@ -71,10 +77,15 @@ new Vue({
       open:true,
       data:[],
     },
+    //后续任务处理情况表数据结构
     followTaskProcessing:{
       title:"后续任务处理",
       open:true,
       data:[
+        {
+          follow_task_name:"后续任务1",
+          handling_measures:""
+        },
         {
           follow_task_name:"",
           handling_measures:"请输入处理措施"
@@ -98,13 +109,31 @@ new Vue({
       },
       
     ],
-    displayOption:1,  //显示选项
-    timeRange:1,  // 时间范围
-    taskStatus:1,  //任务状态
-    timeRangeValue:"", //日期范围 值
+    taskReportDetailShow:false,
+    taskReportDetailData:[],
+    currentDate:"",
+    filterCondition:{
+      conditionOne:1,  //显示选项  1 全部  2 仅显示差异
+      conditionTwo:1,  //时间范围  1 全部  2 范围  时间为timeRange值 ["2020-03-01","2020-03-15"]
+      conditionThree:1, //任务状态 1 全部  2 未开始  3 进行中  4 已完成
+      timeRange:[]  //时间范围 为2时 日期范围所选值 数组["2020-03-01","2020-03-15"]
+    },
+    ifFollowTaskShow:false,  //是否显示后续任务处理表
+    taskReportDetailData:[
+      {
+        plan_duration:"20",  //计划工期
+        actual_duration:"25", //实际工期
+        duration_completion_rate:"90%", //工期完成率
+        plan_quantities:"40",  //计划工程量
+        actual_quantities:"35",  //实际工程量
+        quantities_completion_rate:"76%",  //工程量完成率
+        remaining_duration:"5"  //剩余工期
+      }
+    ],
+    currentClickedTask:null
   },
   created() {
-   
+    this.getCurrentDate()
   },
   mounted() {
     let self = this;
@@ -119,11 +148,22 @@ new Vue({
     })
   },
   methods: {
+    getCurrentDate(){
+      let cDate=new Date()
+      this.currentDate=cDate.getFullYear()+'年'+Number(cDate.getMonth()+1)+'月'+cDate.getDate()+'日'
+    },
     refreshData(){},
     goExit(){},
     goBeforeTaskDetail(){},
     goCurrentTaskControl(){},
-    goTaskReportDetail(){},
+    goTaskReportDetail(){
+      if(this.currentClickedTask!==null){
+        // TODO 发送当前任务id到后台获取任务汇报详情数据并展示
+        this.taskReportDetailShow=true
+      }else{
+        this.$message.error("请先选择任务")
+      }
+    },
     goProgress(){},
     showSelectBox(){
       this.ifSelectBoxShow=!this.ifSelectBoxShow
@@ -150,8 +190,76 @@ new Vue({
     messageConfirm(){
       this.messageBoxShow=false
     },
+    // 三个筛选条件各自改变时 判断时间范围是否为2 且所选日期范围不为空 组合筛选条件发送到后台
+    conditionOneChange(val){
+      if(this.filterCondition.conditionTwo===2){
+        if(this.filterCondition.timeRange.length===0){
+          alert("请选择日期范围")
+        }else{
+          // TODO 发送filterCondition
+          console.log("发送filterCondition",this.filterCondition)
+        }
+      }else{
+        // TODO 发送filterCondition
+        console.log("发送filterCondition",this.filterCondition)
+      }
+    },
+    conditionTwoChange(val){
+      console.log(val)
+      if(val===2){
+        this.$refs.timeRangePicker.focus()
+      }else{
+        // TODO 发送filterCondition
+        console.log("发送filterCondition",this.filterCondition)
+      }
+    },
+    conditionThreeChange(val){
+      if(this.filterCondition.conditionTwo===2){
+        if(this.filterCondition.timeRange.length===0){
+          alert("请选择日期范围")
+        }else{
+          // TODO 发送filterCondition
+          console.log("发送filterCondition",this.filterCondition)
+        }
+      }else{
+        // TODO 发送filterCondition
+        console.log("发送filterCondition",this.filterCondition)
+      }
+    },
     timeRangeChange(value){
       console.log(value)
+      console.log(this.filterCondition)
+    },
+    timeRangeBlur(val){
+      console.log(val)
+      if(this.filterCondition.conditionTwo===2){
+        if(val.value.length===0){
+          alert("请选择日期范围")
+        }else{
+          // TODO 发送filterCondition
+          // alert("发送filterCondition")
+          console.log("发送filterCondition",this.filterCondition)
+        }
+      }
+    },
+    //获取当前点击的任务
+    rowDblclick(row){
+      if(row.task_status==="3"){
+        this.ifFollowTaskShow=true
+      }else{
+        this.ifFollowTaskShow=false
+      }
+      this.currentClickedTask =row
+      // TODO 发送当前点击的任务id到后台 获取相关数据
+    },
+    goSelectFollowTask(){
+      console.log("打开选择后续任务弹窗")
+    },
+    closeTaskReportDetail(){
+      this.taskReportDetailShow=false
+    },
+    goTaskReportListDetail(){
+      console.log("跳转到任务列表详情")
     }
   }
 }).$mount("#progressCAApp")
