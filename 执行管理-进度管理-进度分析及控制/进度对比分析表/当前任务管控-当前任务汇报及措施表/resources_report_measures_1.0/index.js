f91c6dd18cabeeb3784ec527435f22431b8540c7
ivesein <ivesein@outlook.com>
2020-08-18 20:42:33 +0800

    fix resource_report_measures

--- 执行管理-进度管理-进度分析及控制/进度对比分析表/当前任务管控-当前任务汇报及措施表/resources_report_measures_1.0/index.js
+++ 执行管理-进度管理-进度分析及控制/进度对比分析表/当前任务管控-当前任务汇报及措施表/resources_report_measures_1.0/index.js
@@ -124,15 +124,15 @@
                      * @Author: zhang fq
                      * @Date: 2020-08-10
                      * @Description: 根据李阳波要求 修改数据结构和 处理差异化数据 发送到同步接口
-                     * @Date: 2020-08-17
-                     * @Update: 配合李洋波修改自测的多次同步成功发送多次数据的bug
+                     * @Date: 2020-08-18
+                     * @Update: 配合李洋波修改测试组测出的无法保存输入的措施的bug
                      */
                     syncToSM() {
                       // 判断是否已同步成功过一次
                       let ifSyncSuccess = this.tableData.every((v) => {
                         return v.is_deal_with === true;
                       });
-                      if (!ifSyncSuccess) {
+                      if (ifSyncSuccess) {
                         this.$message.info("已同步成功！");
                         return;
                       }
